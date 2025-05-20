8c9c798671d7f89f64c8dfd9f4751e86524a4461
Myles <mylesheller@icloud.com>
2025-05-20 13:11:54 -0400

    rename branches, fix git bug

--- .github/workflows/generate_preset.py
+++ .github/workflows/generate_preset.py
@@ -9,26 +9,31 @@ if len(sys.argv) < 3:
     print("Usage: python generate_charts.py <preset_name> [output_base_dir]")
     sys.exit(1)
 
-feature_preset = sys.argv[1]
+security_preset_name_map = {
+    "low": "easy",
+    "medium": "medium",
+    "high": "hard",
+}
+
+security_level = sys.argv[1]
+preset_name = security_preset_name_map[security_level]
+
 output_base_dir = Path(sys.argv[2])
 
 # Get the repo directory - this will be on the configurable_security branch
-repo_dir = Path('.').absolute()
+repo_dir = Path(".").absolute()
 
-presets = json.loads((repo_dir / 'presets.json').read_text())
-
-if feature_preset not in presets:
-    raise ValueError(f'Feature preset {feature_preset} not found in presets.json')
+presets = json.loads((repo_dir / "presets.json").read_text())
 
 # Get features for this preset
-features = presets[feature_preset].get('features', [])
+features = presets[preset_name].get("features", [])
 
 # Setup context
 context = {k: True for k in features}
-context['project_slug'] = 'ca-k8s-infra'
+context["project_slug"] = "ca-k8s-infra"
 
 # Create output directory for this preset
-output_dir = (output_base_dir / feature_preset).absolute()
+output_dir = (output_base_dir / security_level).absolute()
 output_dir.mkdir(parents=True, exist_ok=True)
 
 # Run cookiecutter with the current directory as template
@@ -37,4 +42,4 @@ cookiecutter(
     output_dir=str(output_dir),
     no_input=True,
     extra_context=context,
-) 
+)
